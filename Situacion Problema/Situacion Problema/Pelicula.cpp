a04dba2890e111389c9aa8af8a5a3ee0cb0d02c7
DiegoGuerraA00835430 <88684991+DiegoGuerraA00835430@users.noreply.github.com>
2022-05-27 10:49:41 -0500

    Update Pelicula.cpp

--- Situacion Problema/Situacion Problema/Pelicula.cpp
+++ Situacion Problema/Situacion Problema/Pelicula.cpp
@@ -1,1 +1,31 @@
 #include "Pelicula.h"
+
+Pelicula::pelicula(){
+  id="";
+  nombre="";
+  duracion=0;
+  genero="";
+}
+
+Pelicula::pelicula(string id, string nombre, int duracion, string genero) {
+  this->id=id;
+  this->nombre=nombre;
+  this->duracion=duracion;
+  this->genero=genero;
+}
+
+void Pelicula::setId(string id){
+  this->id=id;
+}
+
+void Pelicula::setNombre(string nombre){
+  this->nombre=nombre;
+}
+
+void Pelicula::setDuracion(int duracion){
+  this->duracion=duracion;
+}
+
+void Pelicula::setGenero(string genero){
+  this->genero=genero;
+}
